e06ce60edd738f9927c92d56879bcd14a0f0d37c
Fernando-Jun-Saito-Marui <fernandojsm@al.insper.edu.br>
2023-07-25 08:31:45 -0300

    transforma_base 3

--- transforma_base.py
+++ transforma_base.py
@@ -1,13 +1,14 @@
-#2
+#3
 def transforma_base(questoes):
-    if questoes == []:
-        return {}
-    lista_questoes = {'facil':[],'medio':[],'dificil':[]}
+    lista_questoes = {}
     for q in questoes:
         if q['nivel'] == 'facil':
+            lista_questoes['facil'] = []
             lista_questoes['facil'].append(q)
         elif q['nivel'] == 'medio':
+            lista_questoes['medio'] = []
             lista_questoes['medio'].append(q)
         elif q['nivel'] == 'dificil':
+            lista_questoes['dificil'] = []
             lista_questoes['dificil'].append(q)
     return lista_questoes
