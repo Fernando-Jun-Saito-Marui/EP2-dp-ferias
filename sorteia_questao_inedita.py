e7da3749a3ca428d1eda77366d827ed5005d6e75
Fernando-Jun-Saito-Marui <fernandojsm@al.insper.edu.br>
2023-07-28 19:09:28 -0300

    sorteia questao inedita finalizado

--- sorteia_questao_inedita.py
+++ sorteia_questao_inedita.py
@@ -4,4 +4,5 @@ def sorteia_questao_inedita(questoes,nivel,questoes_sorteadas):
     for q in questoes_sorteadas:
         if q == x:
             x = sorteia_questao(questoes,nivel)#sorteia dnv
+    questoes_sorteadas.append(x)
     return x
